055909bd0495ff532b4a97aa7c9cf1a874b69776
samuelGodad <godadsamuel@gmail.com>
2024-10-10 03:53:52 +0300

    making atomic folder structure

--- frontend-nuxt/app.vue
+++ frontend-nuxt/app.vue
@@ -1,5 +1,22 @@
 <template>
-    <div>
-        Hello from the home page !!!
-    </div>
+  <div>
+    Hello from the home page !!!
+    <Button label="Click me" @click="handleClick" />
+  </div>
 </template>
+
+<script>
+import Button from "./stories/Button.vue";
+export default
+ {
+  name: "Home",
+  components: {
+    Button,
+  },
+  methods: {
+    handleClick() {
+      alert("Button clicked");
+    },
+  },
+};
+</script>
--- frontend-nuxt/stories/Button.vue
+++ frontend-nuxt/stories/Button.vue
@@ -1,7 +1,10 @@
 <template>
+  <div class="P-4 bg-blue text-red">
+    <h1 class="text-red">Welcome to My Nuxt App</h1>
+    <Button @click="onClick" label="Click me"></Button>
+  </div>
   <button @click="onClick">{{ label }}</button>
 </template>
-
 <script>
 export default {
   name: "Button",
